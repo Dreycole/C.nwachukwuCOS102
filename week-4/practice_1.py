075a984c51e7cf49a941408def6c4fed6afe931a
Dreycole <pascalchidox18@gmail.com>
2025-03-30 17:01:50 +0100

    my third commit

---
+++ week-4/practice_1.py
@@ -0,0 +1,17 @@
+#python program to swap two cities
+
+#to take inputs from the user
+city_1=input('enter name of city 1:')
+city_2=input('enter name of city 2:')
+
+#create a temporary variable and swap the cities
+temp=city_1
+city_1=city_2
+city_2=temp
+
+print(f"the name of city 1 after swapping is {city_1}")
+print(f"the name of city 2 after swapping is {city_2}")
+
+
+
+
---
+++ week-4/practice_3.py
@@ -0,0 +1,18 @@
+# COUPE DE ESCRIVA 2023: FOOTBALL PICKS
+
+print("welcome to the COUPE DE ESCRIVA 2023:FOOTBALL PICKS \n")
+captain= {'madiba:':'chubby obiora-okafo','blue-jays:':'chris uweh','cirok:':'alexander',
+         'tsg walkers:':'ikechukwu'}
+
+goalkeepers= {'madiba:':'chubby ibiora-okafo'
+             'blue-jays:''olademeji abaniwondea/jefferey awagu',
+             'cirok:':'timileyin pearse/izuako jeremy',
+             'tsg walkers:':'ayomide ojikutu'}
+
+for pick in captain:
+    print(pick,captain[pick])
+
+    print("\n")
+
+    for pick in goalkeepers:
+        print(pick,goalkeepers[pick])
